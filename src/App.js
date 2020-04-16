4c1d419eb2d705abab3f6b6ac15f11dbd65a06ff
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-04-16 18:44:12 +0530

    inline style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React from "react";
 import "./index.css";
 
-function transform(offset) {
-	const cos = Math.cos(offset);
-	const sin = Math.sin(offset);
-	return {
-		transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
-	};
-}
+// function transform(offset) {
+// 	const cos = Math.cos(offset);
+// 	const sin = Math.sin(offset);
+// 	return {
+// 		transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
+// 	};
+// }
 
 class App extends React.Component {
-	state = { styleOne: {}, styleTwo: {} };
+	// state = { styleOne: {}, styleTwo: {} };
 
-	onMouseMove = event => {
-		this.setState({
-			styleOne: transform(-event.clientX / event.clientY),
-			styleTwo: transform(event.clientX / event.clientY)
-		});
-	};
+	// onMouseMove = event => {
+	// 	this.setState({
+	// 		styleOne: transform(-event.clientX / event.clientY),
+	// 		styleTwo: transform(event.clientX / event.clientY)
+	// 	});
+	// };
 
 	render() {
 		return (
-			<div onMouseMove={this.onMouseMove}>
-				<div className="panel" style={this.state.styleOne} />
-				<div className="panel" style={this.state.styleTwo} />
+			<div>
+				<label className="label" htmlFor="name">
+					Enter Name:
+				</label>
+				<input id="name" type="text" />
+				<button style={{backgroundColor:'blue', color:'white'}}>
+					Submit
+				</button>
 			</div>
 		);
 	}
