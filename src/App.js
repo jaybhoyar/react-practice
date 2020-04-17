74f4a8422e28cdc7f81a474b4db2dffcec340cee
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-04-17 19:57:28 +0530

    semantic ui

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
 import React from "react";
 import "./index.css";
 
-// function transform(offset) {
-// 	const cos = Math.cos(offset);
-// 	const sin = Math.sin(offset);
-// 	return {
-// 		transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
-// 	};
-// }
-
 class App extends React.Component {
-	// state = { styleOne: {}, styleTwo: {} };
-
-	// onMouseMove = event => {
-	// 	this.setState({
-	// 		styleOne: transform(-event.clientX / event.clientY),
-	// 		styleTwo: transform(event.clientX / event.clientY)
-	// 	});
-	// };
-
 	render() {
-		return (
-			<div>
-				<label className="label" htmlFor="name">
-					Enter Name:
-				</label>
-				<input id="name" type="text" />
-				<button style={{backgroundColor:'blue', color:'white'}}>
-					Submit
-				</button>
-			</div>
-		);
+		return <div>Hi there</div>;
 	}
 }
 
