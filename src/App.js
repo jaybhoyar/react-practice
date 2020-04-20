67ae493f814e78b23d7dc37a3fb93e2e4c2cc218
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-04-20 14:34:05 +0530

    lifecycle methods

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import Comment from "./Comment";
 import ApprovalCard from "./ApprovalCard";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { lat: null, errorMessage: "" };
+	}
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			(postion) => this.setState({ lat: postion.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
+		);
+	}
 	render() {
-		window.navigator.geolocation.getCurrentPosition((postion, err) => {
-			if (err) console.log(err);
-			console.log(postion);
-		});
 		return (
 			<>
+				{this.state.lat ? (
+					<div>Latitude: {this.state.lat}</div>
+				) : (
+					<div>Error: {this.state.errorMessage}</div>
+				)}
+
 				{/* <ApprovalCard>
             <Comment author="Sam" />
           </ApprovalCard>
