86d179b5a10d1cc980376cb89e71362512d82863
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-04-30 12:04:49 +0530

    Redux Songs App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./App";
-import App from "./Youtube/App";
+// import App from "./Youtube/App";
+import App from "./SongsRedux/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
