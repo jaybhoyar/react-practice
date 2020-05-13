e5c496a205b2b29ea7fee567d702ef106854665c
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-05-13 13:09:17 +0530

    highlight error fields

--- src/Streams/Components/streams/StreamCreate.js
+++ src/Streams/Components/streams/StreamCreate.js
@@ -8,8 +8,9 @@ class StreamCreate extends React.Component {
 		}
 	}
 	renderInput = ({ input, label, meta }) => {
+		const className = `field ${meta.error && meta.touched ? "error" : ""}`;
 		return (
-			<div className="field">
+			<div className={className}>
 				<label>{label}</label>
 				<input type="text" {...input} autoComplete="off" />
 				{this.renderError(meta)}
